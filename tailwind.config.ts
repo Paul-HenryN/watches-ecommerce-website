9f9c4c66f78facd8903732ec75ffc75270300652
paul-henryN <ngpaulhenry@gmail.com>
2024-02-27 17:53:57 +0100

    chore: setup design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,28 @@ const config = {
         "2xl": "1400px",
       },
     },
+    colors: {
+      primary: "#F0B914",
+      "accent-1": "#EA4B48",
+      "accent-2": "#FF8A00",
+      "neutral-900": "#1A1A1A",
+      "neutral-800": "#333333",
+      "neutral-700": "#4D4D4D",
+      "neutral-600": "#666666",
+      "neutral-500": "#808080",
+      "neutral-400": "#999999",
+      "neutral-300": "#B3B3B3",
+      "neutral-200": "#CCCCCC",
+      "neutral-100": "#E6E6E6",
+      "neutral-50": "#F2F2F2",
+      white: "#FFFFFF",
+    },
+    fontWeight: {
+      bold: "600",
+      medium: "500",
+      regular: "400",
+    },
+    fontFamily: {},
     extend: {
       keyframes: {
         "accordion-down": {
